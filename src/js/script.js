d9a1723b6b7044a548fa34b5c8e3b5c7ba3bf9e8
Irina <shishokina.irina@mail.ru>
2020-07-31 20:43:59 +0700

    changed  slider

--- src/js/script.js
+++ src/js/script.js
@@ -17,9 +17,19 @@
 // 	});
 // });
 
-var slider = tns({
+const slider = tns({
 	container: '.carousel__inner',
 	items: 1,
 	slideBy: 'page',
-	autoplay: false
+	autoplay: false,
+	controls: false,
+	nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
 });
