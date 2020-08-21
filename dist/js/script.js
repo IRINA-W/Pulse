07dad95d8d79f15e31b28b20e04fd4ed27ce6778
Irina <shishokina.irina@mail.ru>
2020-08-21 18:29:06 +0700

    плавный скролл для всего документа, доработан gulpfile, папка dist

--- dist/js/script.js
+++ dist/js/script.js
@@ -117,11 +117,11 @@ document.querySelector('.next').addEventListener('click', function () {
 			}
 		});
 
-		$("a[href='#up']").click(function(){
-			const _href = $(this).attr("href");
-			$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-			return false;
-		});
+		// $("a[href='#up']").click(function(){
+		// 	const _href = $(this).attr("href");
+		// 	$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		// 	return false;
+		// });
 
 		new WOW().init();
 
